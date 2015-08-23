532b37f351401394590708c57cc4b5bee7f730d8
Shitanshu Bansal <anshu10virgo@gmail.com>
2015-08-23 11:56:38 +0530

    update run_Analysis

--- run_analysis.R
+++ run_analysis.R
@@ -1,4 +1,4 @@
-
+library (reshape2)
 source("./readTxt.R")
 test_files <- list.files("./UCI HAR Dataset/test/",recursive = FALSE,pattern = "*.txt",full.names = TRUE)
 train_files <- list.files("./UCI HAR Dataset/train/",recursive = FALSE, pattern = "*.txt",full.names = TRUE)
@@ -35,4 +35,12 @@ uci_tidy_header <- sub("BodyBody{1}","Body",uci_tidy_header)
 uci_tidy_header <- sub("Acc{1}","Linear",uci_tidy_header)
 uci_tidy_header <- sub("Gyro{1}","Angular",uci_tidy_header)
 uci_tidy_header <- sub("Mag{1}","Magnitude",uci_tidy_header)
+uci_tidy_header <- gsub("-",".",uci_tidy_header)
 names(uci_tidy_data) <- uci_tidy_header
+
+# Step 5
+uci_measure <- names(uci_tidy_data)[2:67]
+uci_melt_data <- melt(uci_tidy_data, id.vars = c("Subject","Activity"), measure.vars = uci_measure)
+uci_dcast_data <- dcast(uci_melt_data, Subject + Activity ~ variable, mean, na.rm = TRUE)
+
+# Writing the data
