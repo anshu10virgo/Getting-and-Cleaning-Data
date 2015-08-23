f94fe18cfd88e4158b36731692efdb0263232e3b
Shitanshu Bansal <anshu10virgo@gmail.com>
2015-08-23 12:45:30 +0530

    update run_Analysis

--- run_analysis.R
+++ run_analysis.R
@@ -14,10 +14,16 @@ list_of_train_data <- lapply(train_files, readTxt)
 test_data <- do.call(cbind,list_of_test_data)
 train_data <- do.call(cbind,list_of_train_data)
 
+# removing temp variables
+remove(list_of_test_data,list_of_train_data,test_files,train_files,features,activity)
+
 # Step 1
 uci_data <- rbind(test_data,train_data)
 uci_header <- cbind("Subject",t(features_header),"Activity")
 names(uci_data) <- uci_header
+
+# removing temp variables
+remove(test_data,train_data,features_header)
 
 # Step 2
 mean_list <- grepl("mean\\(\\)",uci_header)
@@ -26,8 +32,14 @@ df_list <- mean_list | std_list
 df_list[c(1,563)] <- TRUE
 uci_tidy_data <- uci_data[,uci_header[df_list]]
 
+# removing temp variables
+remove(mean_list,std_list,df_list,uci_header,uci_data)
+
 # Step 3
 uci_tidy_data <- within(uci_tidy_data, Activity <- factor(Activity, labels = activity_labels))
+
+# removing temp variables
+remove(activity_labels)
 
 # Step 4
 uci_tidy_header <- sub("\\(\\)","",names(uci_tidy_data))
@@ -38,9 +50,16 @@ uci_tidy_header <- sub("Mag{1}","Magnitude",uci_tidy_header)
 uci_tidy_header <- gsub("-",".",uci_tidy_header)
 names(uci_tidy_data) <- uci_tidy_header
 
+# removing temp variables
+remove(uci_tidy_header)
+
 # Step 5
 uci_measure <- names(uci_tidy_data)[2:67]
 uci_melt_data <- melt(uci_tidy_data, id.vars = c("Subject","Activity"), measure.vars = uci_measure)
-uci_dcast_data <- dcast(uci_melt_data, Subject + Activity ~ variable, mean, na.rm = TRUE)
+uci_final_dataset <- dcast(uci_melt_data, Subject + Activity ~ variable, mean, na.rm = TRUE)
+
+# removing temp variables
+remove(uci_tidy_data,uci_measure,uci_melt_data)
 
 # Writing the data
+write.table(uci_final_dataset, file = "uci_final_dataset.txt", row.names = FALSE)
